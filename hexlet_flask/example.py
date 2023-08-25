d966798d62956bc44628a3f2af1bd7ba71563c38
seiilaa <vseiilaa@gmail.com>
2023-08-25 15:17:41 +0400

    add add new user functionality

--- hexlet_flask/example.py
+++ hexlet_flask/example.py
@@ -1,7 +1,6 @@
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
-users = ['mike', 'mishel', 'adel', 'keks', 'kamila']
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
@@ -11,32 +10,58 @@ def hello_world():
     return 'Hello again from Flask!'
 
 
-@app.route('/users')
+@app.get('/users')
 def users_index():
+    with open('users.txt', 'r') as repo:
+        users = [json.loads(r) for r in repo.readlines()]
 
-    term = request.args.get('term', default=None)
-    if term is None:
-        filtered_users = users
-        term = ''
-    else:
-        filtered_users = [u for u in users if term in u]
+        search_word = request.args.get('term', default=None)
+        if search_word is None:
+            filtered_users = users
+            search_word = ''
+        else:
+            filtered_users = [
+                u for u in users if 
+                (search_word.lower() in u['name'].lower() or 
+                 search_word.lower() in u['email'].lower())
+                 ]
 
-    return render_template(
-        '/users/index.html',
-        users=filtered_users,
-        search=term
-    )
+        return render_template(
+            '/users/index.html',
+            users=filtered_users,
+            search=search_word
+        )
 
 
-@app.route('/users/<id>')
-def users_html(id):
+@app.get('/users/<id>')
+def user_html(id):
     return render_template(
         '/users/show.html',
         name=id,
     )
 
 
-@app.route('/courses/<id>')
-def courses(id):
-    return f'Course id: {id}'
+@app.post('/users/')
+def user_post():
+    with open('users.txt', 'a') as repo:
+        user = request.form.to_dict()
+        user['id'] = user_id('users.txt')
+        repo.write(json.dumps(user))
+        repo.write("\n")
+        return redirect(url_for('users_index'), code=302)
+    
 
+@app.get('/users/new')
+def users_new():
+    new_user = {'name': '', 'email': ''}
+
+    return render_template(
+        'users/new_user.html',
+        user=new_user)
+
+def user_id(file):
+    with open(file, 'r') as repo:
+        try:
+            return json.loads(repo.readlines()[-1])['id'] + 1
+        except IndexError:
+            return 0
