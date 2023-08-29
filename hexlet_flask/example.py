52d65f975bc8d0084f19f3ce980d9d1c93123513
seiilaa <vseiilaa@gmail.com>
2023-08-29 12:57:21 +0400

    switch the topic of project to Books storage

--- hexlet_flask/example.py
+++ hexlet_flask/example.py
@@ -14,101 +14,101 @@ def route():
     return render_template('index.html')
 
 
-@app.get('/users')
-def get_users():
+@app.get('/books')
+def books_get():
     search_word = request.args.get('term', default=None)
 
-    with open('users.txt', 'r') as repo:
-        users = [json.loads(r) for r in repo.readlines()]
+    with open('books.txt', 'r') as repo:
+        books = [json.loads(r) for r in repo.readlines()]
 
-        filtered_users, search_word = user_search(users, search_word)
+        filtered_books, search_word = search(books, search_word)
         messages = get_flashed_messages(with_categories=True)
         print(messages)
 
         return render_template(
-            '/users/index.html',
-            users=filtered_users,
+            '/books/index.html',
+            books=filtered_books,
             search=search_word,
             messages=messages
         )
 
 
-@app.get('/users/<id>')
-def get_user(id):
-    with open('users.txt', 'r') as repo:
-        users = [json.loads(r) for r in repo.readlines()]
-        for user in users:
-            if int(user['id']) == int(id):
+@app.get('/books/<id>')
+def book_get(id):
+    with open('books.txt', 'r') as repo:
+        books = [json.loads(r) for r in repo.readlines()]
+        for book in books:
+            if int(book['id']) == int(id):
                 return render_template(
-                '/users/show.html',
-                user=user
+                '/books/show.html',
+                book=book
                 )
         return render_template('404.html'), 404
 
 
-@app.post('/users/')
-def post_user():
-    with open('users.txt', 'a') as repo:
-        user = request.form.to_dict()
+@app.post('/books/')
+def book_post():
+    with open('books.txt', 'a') as repo:
+        book = request.form.to_dict()
 
-        errors = validate(user)
+        errors = validate(book)
 
         if errors:
             return render_template(
-                'users/new_user.html',
-            user=user,
+                'books/new_book.html',
+            book=book,
             errors=errors
             ), 422
 
-        user['id'] = generate_user_id('users.txt')
-        repo.write(json.dumps(user))
+        book['id'] = generate_id('books.txt') # type: ignore
+        repo.write(json.dumps(book))
         repo.write("\n")
-        flash('User has been created', 'success')
+        flash('Book has been added', 'success')
 
-        return redirect(url_for('get_users'), code=302)
+        return redirect(url_for('books_get'), code=302)
     
 
-@app.get('/users/new')
-def create_user():
-    user = {'name': '', 'email': ''}
+@app.get('/books/new')
+def book_create():
+    book = {'name': '', 'summary': ''}
     errors = []
     return render_template(
-        'users/new_user.html',
-        user=user,
+        'books/new_book.html',
+        book=book,
         errors=errors)
 
+
 @app.errorhandler(404)
-# inbuilt function which takes error as parameter
 def not_found(e):
   return render_template("404.html")
 
 
-def generate_user_id(file):
+def generate_id(file):
     with open(file, 'r') as repo:
         try:
             return json.loads(repo.readlines()[-1])['id'] + 1
         except IndexError:
             return 0
         
-def user_search(users, search_word):
+def search(data, search_word):
     search_word = request.args.get('term', default=None)
     if search_word is None:
-        filtered_users = users
+        filtered_data = data
         search_word = ''
     else:
-        filtered_users = [
-            u for u in users if 
+        filtered_data = [
+            u for u in data if 
             (search_word.lower() in u['name'].lower() or 
-             search_word.lower() in u['email'].lower())
+             search_word.lower() in u['summary'].lower())
              ]
-    return filtered_users, search_word
+    return filtered_data, search_word
 
 def validate(data):
     errors = {}
     if len(data['name']) < 2:
         errors['name'] = 'Name is too short!'
-    if '@' not in data['email']:
-        errors['email'] = 'Invalid email format!'
+    if len(data['summary']) == 0:
+        errors['summary'] = "Summary can't be empty!"
 
     return errors 
 
